ce5faa2546765d32fb5c84e918a8b3380b1a47af
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-03-15 23:10:01 +0900

    [Feat] Get user's numbers

--- game.cpp
+++ game.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 
+extern int strikes;
+extern int balls;
+
 void getNumbers(int array[], int size);
 void getAnswerNumbers(int array[], int SIZE);
 
 void play(int SIZE) {
-    int answerNumbers[SIZE];
+    int answerNumbers[SIZE], userNumbers[SIZE];
     getAnswerNumbers(answerNumbers, SIZE);
+
+    while (strikes < SIZE) {
+        getUserNumbers(userNumbers, SIZE);
+    }
 }
 
 void getAnswerNumbers(int array[], int SIZE) {
     std::cout << "Enter a answer: ";
+    getNumbers(array, SIZE);
+}
+
+void getUserNumbers(int array[], int SIZE) {
+    std::cout << "Enter a guess: ";
     getNumbers(array, SIZE);
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 const int SIZE = 3;
+int strikes = 0, balls = 0;
 void play(int);
 
 int main() {
