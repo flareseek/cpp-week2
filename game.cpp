1cc3223ef201369b6d77f575cc126da548a12ef4
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-03-15 23:20:20 +0900

    [FIX] Get user's numbers

--- game.cpp
+++ game.cpp
@@ -5,6 +5,8 @@ extern int balls;
 
 void getNumbers(int array[], int size);
 void getAnswerNumbers(int array[], int SIZE);
+void getUserNumbers(int arrya[], int SIZE);
+void setScores(int userNumbers[], int answerNumbers[], int size);
 
 void play(int SIZE) {
     int answerNumbers[SIZE], userNumbers[SIZE];
@@ -12,6 +14,7 @@ void play(int SIZE) {
 
     while (strikes < SIZE) {
         getUserNumbers(userNumbers, SIZE);
+        setScores(userNumbers, answerNumbers, SIZE);
     }
 }
 
@@ -23,4 +26,27 @@ void getAnswerNumbers(int array[], int SIZE) {
 void getUserNumbers(int array[], int SIZE) {
     std::cout << "Enter a guess: ";
     getNumbers(array, SIZE);
+}
+
+void setScores(int userNumbers[], int answerNumbers[], int size) {
+    int _strikes = 0, _balls = 0;
+    for (int userNumberIndex = 0; userNumberIndex < size; userNumberIndex++) {
+        const int currentUserNumber = userNumbers[userNumberIndex];
+
+        if (currentUserNumber == answerNumbers[userNumberIndex]) {
+            _strikes++;
+            continue;
+        }
+
+        for (int answerNumberIndex = 0; answerNumberIndex < size; answerNumberIndex++) {
+            if (answerNumberIndex == userNumberIndex) continue;
+
+            const int currentAnswerNumber = answerNumbers[answerNumberIndex];
+            if (currentAnswerNumber != currentUserNumber) continue;
+            _balls++;
+            break;
+        }
+    }
+    strikes = _strikes;
+    balls = _balls;
 }
